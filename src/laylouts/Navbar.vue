f498e4892f814039676e3c5747e96f2414bd3c11
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2022-12-24 18:30:28 +0500

    changed all file

--- src/laylouts/Navbar.vue
+++ src/laylouts/Navbar.vue
@@ -48,9 +48,9 @@
           ></i>
 
           <ul class="dropdown" :class="{ active: is_dropdawn_open }" >
-            <li @click="uzbb"> <router-link to="catalog">UZB</router-link> </li>
+            <li @click="uzbb"> UZB </li>
             <li @click="russ">RUS</li>
-            <li @click="engg">ENG</li>
+            <li @click="engg"><router-link to="catalog">ENG</router-link></li>
           </ul>
         </div>
 
